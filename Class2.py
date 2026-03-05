8a91bcfd9bcff3daef1322b7eac50a5d0c5f59b5
Souptikbej <bejsouptik@gmail.com>
2026-03-05 23:37:49 +0530

    Day2

---
+++ Class2.py
@@ -0,0 +1,43 @@
+print("helllo world ")
+#i am akarsh vyas 
+#and I am teaching python  #comments 
+
+
+#variables 
+
+a = 12
+b = 56
+
+chacha = 45
+papa = 46
+
+# Naming Conventions 
+#camel case 
+
+akarshVyas = 12 
+
+#pascal case 
+
+AkarshVyas = 34
+
+#Snake case 
+
+akarsh_vyas = 67
+
+#1 error 
+
+hello = 25
+
+
+
+#we store data types in variables 
+
+# a = 'wedtgfasdjgbasdg !$@#%#$*%#$*(^ ) 1234235346457'
+
+# print(type(a))
+
+
+b = True
+c = False
+
+print(type(b))
---
+++ Class3.py
@@ -0,0 +1,10 @@
+a= "Hello I am Data Scientist"
+
+#Extract Hello
+print(a[0:5:1])
+#Extract Data
+print(a[11:16:1])
+#Extract Scientist
+print(a[16::1])
+
+print(ord('A'))
