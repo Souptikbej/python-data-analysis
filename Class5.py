4ee255edc427798103f8de72771b3a56cab89f6b
Souptikbej <bejsouptik@gmail.com>
2026-03-06 22:44:15 +0530

    Day4

---
+++ Class5.py
@@ -0,0 +1,25 @@
+#Comparision Operator 
+
+# == , != , > , < , <= , >=
+
+print(12!=12)
+
+print(12!=12.1)
+
+print(12>13)
+print(12<12)
+
+print(12<=12)
+print(12>=12)
+
+
+print(12<13)
+
+print((12==12)==True)
+
+
+
+#Logical operators
+# and , or
+
+print(12==12 and 12!=13 and 56>50 and 100<200 )
