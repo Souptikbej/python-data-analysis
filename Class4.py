5358064030c6a044fe85ffbe440d83c5648657cc
Souptikbej <bejsouptik@gmail.com>
2026-03-06 21:37:51 +0530

    Day4

---
+++ Class4.py
@@ -0,0 +1,21 @@
+#arithmetic operators
+# +,-,*,/,//,**,%
+#BODMAS stands for Brackets, Orders (powers/roots), Division, Multiplication, Addition, and Subtraction
+
+a=12
+b=34
+print(a+b)    
+
+print(10 ** 2)
+
+
+a/=3
+print(a)
+a=12
+a//=3
+print(a)
+
+#what is the result type of a comparison operation in python with example
+x=10
+u=10
+print(x==u)
