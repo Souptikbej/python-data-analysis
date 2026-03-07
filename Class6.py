4cc0d2db8fa87a75f2d2abd3fcf54db7db2faba0
Souptikbej <bejsouptik@gmail.com>
2026-03-07 11:40:18 +0530

    Day5

---
+++ Class6.py
@@ -0,0 +1,23 @@
+#Question 1
+a=int(input("Enter your fisrt number : "))
+b=int(input("Enter your second number : "))
+
+if a>b:
+  print(f"{a} is greater than.")
+elif b>a:
+  print(f"{b} is greater than.")
+else:
+  print(f"{a} is equal to {b}")
+
+
+
+#Question 2 
+gen = input("Please tell me your Gender (m/f): ")
+if gen =='m':
+  print("Hello Sir how are you .")
+else:
+  print("Hello Mam how are you .")
+
+
+#Question 3
+
