aaceb8d9fc384a9843126e5a9f15386f99b5db22
Souptikbej <bejsouptik@gmail.com>
2026-03-14 18:25:15 +0530

    Day8

--- Class7.py
+++ Class7.py
@@ -107,3 +107,20 @@ for i in range(0,len(r)):
   s+=int(r[i])
 print(s)
 '''
+
+num = input("")
+
+even_count = 0
+odd_count = 0
+
+for digit in num:
+    if digit == '-':
+        continue
+    
+    if int(digit) % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+print("Even: ", even_count)
+print("Odd: ", odd_count)
