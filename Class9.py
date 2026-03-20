ea5200db9c7e3021214de1f329cf5d4e809376dc
Souptikbej <bejsouptik@gmail.com>
2026-03-21 00:50:24 +0530

    Day13

--- Class9.py
+++ Class9.py
@@ -22,8 +22,71 @@ def addition(a,b):
   print(a+b)
 addition(10,10)
 '''
+#Welcome Message
+'''
 def welcome_message(name):
     # Write your code here
     print(f"Welcome, {name}!")
 name=input("")
 welcome_message(name)
+'''
+#Multiplication table Function
+'''
+def print_table(n):
+    if isinstance(n, int):
+        for i in range(1, 11):
+            print(f"{n} x {i} = {n*i}")
+print_table(5)
+'''
+#Count Digit Function
+'''
+def count_digits(n):
+    if not isinstance(n,int):
+        print(0)
+    n=abs(n)
+    if n==0:
+        print(1)
+    else:
+        c=0
+        while n>0:
+            c+=1
+            n=n//10
+        print(c)
+count_digits(12345)
+'''
+#Sum of First n Naturals
+'''
+def sum_of_naturals(n):
+    s=0
+    for i in range (1,n+1):
+        s=s+i
+    print(s)
+sum_of_naturals(5)
+'''
+#Reverse Number Function
+'''
+def reverse_number(n):
+    if isinstance(n, int):
+        s=0
+        while n>0:
+            r=n%10
+            s=s*10+r
+            n=n//10
+    print(s)
+reverse_number(123456)
+'''
+#Armstrong Number Function
+def check_armstrong(n):
+    if isinstance(n,int):
+        p=len(str(n))
+        copy=n
+        s=0
+        while n>0:
+            r=n%10
+            s=s+r**p
+            n=n//10
+        if copy==s:
+            print("Armstrong Number")
+        else:
+            print("Not Armstrong Number")
+check_armstrong(153)
