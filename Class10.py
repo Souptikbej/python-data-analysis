c71b06ba96882b286f1b05b50569a2da849e41cd
Souptikbej <bejsouptik@gmail.com>
2026-03-23 23:56:02 +0530

    Day16

--- Class10.py
+++ Class10.py
@@ -49,3 +49,73 @@ else:
   print("list is sorted")
 '''
 #Left Rotation by 1
+'''
+a=[1,2,3,4,5,6,7,8,9,10]
+for i in range(len(a)-1):
+  a[i],a[i+1]=a[i+1],a[i]
+print(a)
+'''
+#Right Rotation by 1
+'''
+a=[1,2,3,4,5,6,7,8,9,10]
+for i in range(len(a)-1,0,-1):
+  a[i],a[i-1]=a[i-1],a[i]
+print(a)
+'''
+#K times rotation 
+'''
+k=int(input("Enter how many time u wants to rotate : "))
+a=[10,20,30,40,50,60,70,80,90,100]
+print(f"Before Rotation List Looks Like : {a}")
+for j in range(1,k+1):
+  for i in range(len(a)-1):
+    a[i],a[i+1]=a[i+1],a[i]
+  print(f"{j} time rotation looks : {a}")
+'''
+#Reverse the entire list without using extra space 
+'''
+a=[10,20,30,40,50,60,70,80,90]
+print(f"Before Rotation List Looks Like : {a}")
+l=len(a)-1
+for i in range(len(a)//2):
+  a[i],a[l]=a[l],a[i]
+  l-=1
+print(f"After Rotation List Looks Like : {a}")
+'''
+#Linear Search
+'''
+a=[1,25,32,96,35,74,55,66,38,94,50]
+search=35
+for i in range(len(a)):
+  if search==a[i]:
+    print(f"Element found at index of {i}")
+    break
+else:
+  print("Element Not Found")
+'''
+#Binary Search
+'''
+a=[10,20,30,40,50,60,70,80,90]
+search=40
+start=0
+last=len(a)-1
+while start<=last:
+  mid=(start+last)//2
+  if a[mid]==search:
+    print(f"Elememt Found at index {mid}")
+    break
+  elif a[mid]>search:
+    last=mid-1
+  elif a[mid]<search:
+    start=mid+1
+else:
+  print("Not found")
+'''
+#Bubble Sort
+a=[34,26,89,123,18,12,11]
+
+
+
+
+
+#Selection Sort
