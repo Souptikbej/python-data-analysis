259e8287a42c62a231e89d639617a563cd271573
Souptikbej <bejsouptik@gmail.com>
2026-03-29 23:57:15 +0530

    Day21

--- Class13.py
+++ Class13.py
@@ -5,3 +5,11 @@ print(d["Bro"])#accessing the  value
 d["Bro"]=500 #Change the value
 
 print(d["Bro"])#Show  the  changed value
+
+#Create dictionary first Way
+d=dict(Name="Souptik Bej",Age=23,Gender="Male")
+print(d)
+
+#Create dictionary Second Way
+d=dict([("Name","Souptik" ),("Age",23)])
+print(d)
