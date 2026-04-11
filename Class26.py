b705d4325343312e0717722c02eda48eea00a617
Souptikbej <bejsouptik@gmail.com>
2026-04-11 22:30:30 +0530

    Day27

--- Class26.py
+++ Class26.py
@@ -1,1 +1,20 @@
 #Abstraction 
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+  @abstractmethod
+  def sound(self):
+    print("Yhea you use me Because i am main Branch")
+
+
+class Dog (Animal):
+  def sound(self):
+    return super().sound()
+  def hello(self):
+    print("I am franchicy")
+
+
+obj=Dog()
+obj.sound()
+obj.hello()
