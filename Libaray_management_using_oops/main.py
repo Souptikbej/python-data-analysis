14472c113a0d09ed51cac9fb4cd5e1eb4c59e1ea
Souptikbej <bejsouptik@gmail.com>
2026-04-13 11:03:23 +0530

    Day29

--- Libaray_management_using_oops/main.py
+++ Libaray_management_using_oops/main.py
@@ -5,11 +5,29 @@ from pathlib import Path
 from datetime import datetime
 
 class Library:
+  databse="Libaray_management_using_oops/Library.json"
+  data={"Books":[],"Members":[]}
+
+  #Load exixting data to JSON or create your JSON
+  if Path(databse).exists():
+    with open(databse,"r") as f:
+      content=f.read().strip()
+      if content:
+        data=json.loads(content)
+  else:
+    with open(databse,"w") as f:
+      json.dump(data,f,indent=4)
+
   def unique_id(Prefix ='B'):
     u_id=""
     for i in range(5):
       u_id+=random.choice(string.ascii_uppercase +string.digits)
     return Prefix + "-" + u_id
+  
+  @classmethod
+  def save_data(cls):
+    with open(cls.databse,'w') as f:
+      json.dump(cls.data,f,indent=4,default=str)
 
 
   def add_book(self):
@@ -25,13 +43,58 @@ class Library:
       "book_avalible":self.copies,
       "book_adds_on":datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
-    print(book_dict)
+    Library.data["Books"].append(book_dict)
+    Library.save_data()
 
 
+  def list_books(self):
+    if not Library.data["Books"]:
+      print("Sorry no books found")
+      return
+    for b in Library.data["Books"]:
+      print(f"{b['id']:12} {b['book_name']:25} {b['book_author']:12} {b['book_copies']:12}")
+  
+
+  def add_member(self):
+    self.name=input("Enter the name :")
+    self.email=input("Enter the email id :")
+    member_dict={
+      "id":Library.unique_id("M"),
+      "member_name":self.name,
+      "member_email":self.email,
+      "borowed":[],
+      "member_adds_on":datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+    Library.data["Members"].append(member_dict)
+    Library.save_data()
+    print("Member Added Successfully......")
+
+  def list_members(self):
+      if not Library.data["Members"]:
+        print("Sorry no Members found")
+        return
+      for m in Library.data["Members"]:
+        print(f"{m['id']:12} {m['member_name']:25} {m['member_email']:12}")
+        if not m['borowed']:
+          print("No Borowed Book")
+        else:
+          print(f"{m['borowed']:12}")
 
 
+  def borrowed_book(self):
+    member_id=input("Enter Member id :").strip()
+    members=[i for i in Library.data["Members"] if i['id']==member_id]
+    if not members:
+      print("No such Id exist")
+    member=members[0]
+    book_id=input("Enter Book id :").strip()
+    books=[i for i in Library.data["Books"] if i['id']==book_id]
+    if not books:
+      print("No such Id exist")
+    book=members[0]
 
 obj=Library()
+
 print("="*50)
 print("Library Management System")
 print("="*50)
@@ -48,3 +111,13 @@ choise=int(input("What you want to do :"))
 
 if choise==1:
   obj.add_book()
+if choise==2:
+  obj.list_books()
+if choise==3:
+  obj.add_member()
+if choise==4:
+  obj.list_members()
+if choise==5:
+  obj.borrowed_book()
+if choise==6:
+  obj.return_book()
